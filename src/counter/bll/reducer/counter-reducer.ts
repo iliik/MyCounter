3edbe759b46425a71fca49687e4b6d7bf80a8295
kosel <koselevicila@gmail.com>
2023-03-28 16:55:56 +0300

    refactor-reducer

--- src/counter/bll/reducer/counter-reducer.ts
+++ src/counter/bll/reducer/counter-reducer.ts
@@ -74,19 +74,19 @@ export const incValuesTC = (value: number) => (dispatch: Dispatch) => {
     dispatch(setAddNumAC())
 }
 export const incStartNumTC = (startValue: number) => (dispatch: Dispatch) => {
-    localStorage.setItem('counterValue', JSON.stringify(startValue))
+    localStorage.setItem('counterStartValue', JSON.stringify(startValue))
     dispatch(setAddStartNumAC(startValue))
 }
 export const incMaxNumTC = (maxValue: number) => (dispatch: Dispatch) => {
-    localStorage.setItem('counterValue', JSON.stringify(maxValue))
+    localStorage.setItem('counterMaxValue', JSON.stringify(maxValue))
     dispatch(setAddMaxNumAC(maxValue))
 }
 export const incResetTC = (value:number ) => (dispatch: Dispatch) => {
-    localStorage.setItem('counterValue', JSON.stringify(value))
+    localStorage.setItem('counterResetValue', JSON.stringify(value))
     dispatch(setResetAC(value))
 }
 
 export const deleteTC = (value:number ) => (dispatch: Dispatch) => {
-    localStorage.setItem('counterValue', JSON.stringify(value))
+    localStorage.setItem('counterDelete', JSON.stringify(value))
     dispatch(deleteAC(value))
 }
